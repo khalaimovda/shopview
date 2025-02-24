9d41ba15668590787d79d66ad8cada6ee96ef8ca
khalaimovda <halaimov_15@mail.ru>
2025-02-24 22:00:37 +0100

    Project initialization

---
+++ src/test/java/com/github/khalaimovda/shopview/ShopViewApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.khalaimovda.shopview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ShopViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
