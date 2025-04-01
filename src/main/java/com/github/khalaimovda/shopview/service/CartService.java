b6b13c61244a03bd5c11ac88155099c5a5bf1541
khalaimovda <halaimov_15@mail.ru>
2025-04-01 12:45:50 +0300

    CartService to reactive

--- src/main/java/com/github/khalaimovda/shopview/service/CartService.java
+++ src/main/java/com/github/khalaimovda/shopview/service/CartService.java
@@ -1,13 +1,12 @@
 package com.github.khalaimovda.shopview.service;
 
 import com.github.khalaimovda.shopview.dto.OrderDetail;
-
-import java.util.Optional;
+import reactor.core.publisher.Mono;
 
 public interface CartService {
-    Optional<OrderDetail> getCart();
-    void addProductToCart(Long productId);
-    void decreaseProductInCart(Long productId);
-    void removeProductFromCart(Long productId);
-    void checkout();
+    Mono<OrderDetail> getCart();
+    Mono<Void> addProductToCart(Long productId);
+    Mono<Void> decreaseProductInCart(Long productId);
+    Mono<Void> removeProductFromCart(Long productId);
+    Mono<Void> checkout();
 }
--- src/main/java/com/github/khalaimovda/shopview/service/CartServiceImpl.java
+++ src/main/java/com/github/khalaimovda/shopview/service/CartServiceImpl.java
@@ -6,15 +6,12 @@ import com.github.khalaimovda.shopview.model.Product;
 import com.github.khalaimovda.shopview.repository.OrderRepository;
 import com.github.khalaimovda.shopview.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 
 @Service
@@ -28,80 +25,86 @@ public class CartServiceImpl implements CartService {
 
     @Override
     @Transactional
-    @Cacheable(value = "orders", key = "'cart'")
-    public Optional<OrderDetail> getCart() {
-        Optional<Order> optionalOrder = orderRepository.findByIsActiveTrue().blockOptional();
-        return optionalOrder.map(orderService::getOrderDetail);
+//    @Cacheable(value = "orders", key = "'cart'")
+    public Mono<OrderDetail> getCart() {
+        return orderRepository
+            .findByIsActiveTrue()
+            .map(orderService::getOrderDetail);
     }
 
     @Override
     @Transactional
-    @Caching(evict = {
-        @CacheEvict(value = "products", allEntries = true),
-        @CacheEvict(value = "orders", allEntries = true)
-    })
-    public void addProductToCart(Long productId) {
-        Product product = getProductByIdOrNoSuchElementException(productId);
-        Order activeOrder = getOrCreateActiveOrder();
-        orderProductService.addProductToOrder(activeOrder, product);
+//    @Caching(evict = {
+//        @CacheEvict(value = "products", allEntries = true),
+//        @CacheEvict(value = "orders", allEntries = true)
+//    })
+    public Mono<Void> addProductToCart(Long productId) {
+        return getProductByIdOrNoSuchElementException(productId)
+            .flatMap(product -> getOrCreateActiveOrder()
+                .flatMap(cart -> orderProductService.addProductToOrder(cart, product)));
     }
 
     @Override
     @Transactional
-    @Caching(evict = {
-        @CacheEvict(value = "products", allEntries = true),
-        @CacheEvict(value = "orders", allEntries = true)
-    })
-    public void decreaseProductInCart(Long productId) {
-        Product product = getProductByIdOrNoSuchElementException(productId);
-        Order activeOrder = getActiveOrderOrNoSuchElementException();
-        orderProductService.decreaseProductInOrder(activeOrder, product);
+//    @Caching(evict = {
+//        @CacheEvict(value = "products", allEntries = true),
+//        @CacheEvict(value = "orders", allEntries = true)
+//    })
+    public Mono<Void> decreaseProductInCart(Long productId) {
+        return getProductByIdOrNoSuchElementException(productId)
+            .flatMap(product -> getActiveOrderOrNoSuchElementException()
+                .flatMap(cart -> orderProductService.decreaseProductInOrder(cart, product)));
     }
 
     @Override
     @Transactional
-    @Caching(evict = {
-        @CacheEvict(value = "products", allEntries = true),
-        @CacheEvict(value = "orders", allEntries = true)
-    })
-    public void removeProductFromCart(Long productId) {
-        Product product = getProductByIdOrNoSuchElementException(productId);
-        Order activeOrder = getActiveOrderOrNoSuchElementException();
-        orderProductService.removeProductFromOrder(activeOrder, product);
+//    @Caching(evict = {
+//        @CacheEvict(value = "products", allEntries = true),
+//        @CacheEvict(value = "orders", allEntries = true)
+//    })
+    public Mono<Void> removeProductFromCart(Long productId) {
+        return getProductByIdOrNoSuchElementException(productId)
+            .flatMap(product -> getActiveOrderOrNoSuchElementException()
+                .flatMap(cart -> orderProductService.removeProductFromOrder(cart, product)));
     }
 
     @Override
     @Transactional
-    @Caching(evict = {
-        @CacheEvict(value = "products", allEntries = true),
-        @CacheEvict(value = "orders", allEntries = true)
-    })
-    public void checkout() {
-        Optional<Order> optionalOrder = orderRepository.findByIsActiveTrue().blockOptional();
-        Order order = optionalOrder.orElseThrow(() -> new NoSuchElementException("Cart does not exist"));
-        validateCartIsNotEmpty(order);
-        order.setIsActive(false);
-        orderRepository.save(order);
+//    @Caching(evict = {
+//        @CacheEvict(value = "products", allEntries = true),
+//        @CacheEvict(value = "orders", allEntries = true)
+//    })
+    public Mono<Void> checkout() {
+        return orderRepository
+            .findByIsActiveTrue()
+            .map(cart -> {
+                validateCartIsNotEmpty(cart); // todo: Доработать, когда orderService будет исправлен
+                return cart;
+            }).flatMap(
+                cart -> {
+                    cart.setIsActive(false); // checkout
+                    return orderRepository.save(cart).then();
+                }
+            );
     }
 
     @Transactional
-    private Order getOrCreateActiveOrder() {
-        Optional<Order> activeOrder = orderRepository.findByIsActiveTrue().blockOptional();
-        return activeOrder.orElseGet(() -> orderRepository.save(new Order()).block());
+    private Mono<Order> getOrCreateActiveOrder() {
+        return orderRepository
+            .findByIsActiveTrue()
+            .switchIfEmpty(orderRepository.save(new Order()));
     }
 
-    private Product getProductByIdOrNoSuchElementException(Long productId) {
-        Optional<Product> optionalProduct = productRepository.findById(productId).blockOptional();
-        return optionalProduct.orElseThrow(
-            () -> new NoSuchElementException(String.format("Product with id %s is not found", productId))
-        );
+    private Mono<Product> getProductByIdOrNoSuchElementException(Long productId) {
+        return productRepository.findById(productId)
+            .switchIfEmpty(Mono.error(
+                new NoSuchElementException(String.format("Product with id %s is not found", productId))));
     }
 
-    private Order getActiveOrderOrNoSuchElementException() {
-        Optional<Order> optionalActiveOrder = orderRepository.findByIsActiveTrue().blockOptional();
-        return optionalActiveOrder.orElseThrow(
-            () -> new NoSuchElementException("Active order is not found")
-        );
+    private Mono<Order> getActiveOrderOrNoSuchElementException() {
+        return orderRepository
+            .findByIsActiveTrue()
+            .switchIfEmpty(Mono.error(new NoSuchElementException("Active order is not found")));
     }
 
     private void validateCartIsNotEmpty(Order order) {
