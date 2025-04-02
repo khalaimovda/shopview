90fe8fd3e3c0aef5bb9331ead40e1f213fd30a98
khalaimovda <halaimov_15@mail.ru>
2025-04-02 14:00:58 +0300

    ImageService unit tests

--- src/test/java/com/github/khalaimovda/shopview/service/ImageFileSystemServiceTest.java
+++ src/test/java/com/github/khalaimovda/shopview/service/ImageFileSystemServiceTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.codec.multipart.FilePart;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,11 +20,18 @@ import java.nio.file.Paths;
 
 import static com.github.khalaimovda.shopview.utils.ImageUtils.createRandomBytes;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ImageFileSystemServiceTest {
     @TempDir
     private Path tempDir;
+
+    @Mock
+    FilePart imageFile;
+
+    @Captor
+    ArgumentCaptor<Path> pathCaptor;
 
     private ImageServiceProperties properties;
     private ImageService imageService;
@@ -33,21 +44,22 @@ class ImageFileSystemServiceTest {
         imageService = new ImageFileSystemService(properties);
     }
 
-//    @Test
-//    void testSaveImage() throws IOException {
-//        byte[] imageBytes = createRandomBytes();
-//        MultipartFile imageFile = new MockMultipartFile(
-//            "image", "test.jpg", "image/jpeg", imageBytes
-//        );
-//
-//        String storedFilename = imageService.saveImage(imageFile);
-//
-//        Path savedFilePath = Paths.get(properties.getUploadDir()).resolve(storedFilename);
-//        assertTrue(Files.exists(savedFilePath));
-//
-//        byte[] savedContent = Files.readAllBytes(savedFilePath);
-//        assertArrayEquals(imageBytes, savedContent);
-//    }
+    @Test
+    void testSaveImage() throws IOException {
+        String imageName = "test_image.jpg";
+        when(imageFile.filename()).thenReturn(imageName);
+        when(imageFile.transferTo(any(Path.class))).thenReturn(Mono.empty());
+
+        Mono<String> storedFilename = imageService.saveImage(imageFile);
+
+        StepVerifier
+            .create(storedFilename)
+            .expectNextMatches(filename -> filename.endsWith(imageName))
+            .verifyComplete();
+        verify(imageFile, times(1)).transferTo(pathCaptor.capture());
+        assertTrue(pathCaptor.getValue().getFileName().toString().endsWith(imageName));
+
+    }
 
     @Test
     void testGetImagePath() {
@@ -69,7 +81,10 @@ class ImageFileSystemServiceTest {
         Files.write(imagePath, imageBytes);
         assertTrue(Files.exists(imagePath));
 
-        imageService.deleteImage("test_image.jpg");
+        Mono<Void> monoResult = imageService.deleteImage("test_image.jpg");
+        StepVerifier
+            .create(monoResult)
+            .verifyComplete();
         assertFalse(Files.exists(imagePath));
     }
 }
