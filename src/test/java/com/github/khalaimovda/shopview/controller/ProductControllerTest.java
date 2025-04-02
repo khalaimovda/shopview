641470d437757453b35945223bb1886435effd70
khalaimovda <halaimov_15@mail.ru>
2025-04-02 18:27:12 +0300

    ProductController tests

--- src/test/java/com/github/khalaimovda/shopview/controller/ProductControllerTest.java
+++ src/test/java/com/github/khalaimovda/shopview/controller/ProductControllerTest.java
@@ -11,35 +11,35 @@ import org.mapstruct.factory.Mappers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static com.github.khalaimovda.shopview.utils.ImageUtils.createRandomBytes;
 import static com.github.khalaimovda.shopview.utils.ProductUtils.generateRandomProduct;
 import static com.github.khalaimovda.shopview.utils.ProductUtils.generateRandomProducts;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(ProductController.class)
+@WebFluxTest(ProductController.class)
 public class ProductControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @MockitoBean
     private ProductService productService;
@@ -63,17 +63,25 @@ public class ProductControllerTest {
             .map(product -> productMapper.toProductListItem(product, "/image/path/1.jpg", 3))
             .toList();
         Pageable pageable = PageRequest.of(page, size);
-        when(productService.getAllProducts(any(), any())).thenReturn(
-            new PageImpl<>(productListItems, pageable, total)
-        );
+        when(productService.getAllProducts(anyString(), any(Pageable.class)))
+            .thenReturn(Mono.just(new PageImpl<>(productListItems, pageable, total)));
 
-        mockMvc.perform(get("/products")
-                .param("search", search)
-                .param("page", String.valueOf(page))
-                .param("size", String.valueOf(size)))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("text/html;charset=UTF-8"))
-            .andExpect(view().name("products"));
+        webTestClient.get()
+            .uri(builder -> builder
+                .path("/products")
+                .queryParam("search", search)
+                .queryParam("page", page)
+                .queryParam("size", size)
+                .build())
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.TEXT_HTML)
+            .expectBody(String.class)
+            .consumeWith(response -> {
+                String body = response.getResponseBody();
+                assertNotNull(body);
+                assertTrue(body.contains("products"));
+            });
 
         verify(productService, times(1)).getAllProducts(eq(search), pageableCaptor.capture());
         assertAll(
@@ -85,41 +93,55 @@ public class ProductControllerTest {
 
     @Test
     void testCreateProduct() throws Exception {
-        MockMultipartFile imageFile = new MockMultipartFile("image", "test.jpg", "image/jpeg", new byte[100]);
-        ProductCreateForm form = ProductCreateForm.builder()
-            .name("TestName")
-            .description("TestDescription")
-            .image(imageFile)
-            .price(new BigDecimal("82.13"))
-            .build();
+        String name = "TestName";
+        String description = "TestDescription";
+        BigDecimal price = new BigDecimal("82.13");
 
-        mockMvc.perform(multipart("/products")
-                .file("image", form.getImage().getBytes())
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .formField("name", form.getName())
-                .formField("description", form.getDescription())
-                .formField("price", String.valueOf(form.getPrice())))
-            .andExpect(status().isCreated());
+        when(productService.createProduct(any(ProductCreateForm.class)))
+            .thenReturn(Mono.empty());
+
+        webTestClient.post()
+            .uri("/products")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(BodyInserters.fromMultipartData("name", name)
+                .with("description", description)
+                .with("price", price.toString())
+                .with("image", new HttpEntity<>(
+                    new ByteArrayResource(createRandomBytes()) {
+                        @Override
+                        public String getFilename() {
+                            return "test-image.jpg";
+                        }
+                    })))
+            .exchange()
+            .expectStatus().isCreated();
 
         verify(productService, times(1)).createProduct(productCreateFormCaptor.capture());
         assertAll(
-            () -> assertEquals(form.getName(), productCreateFormCaptor.getValue().getName()),
-            () -> assertEquals(form.getDescription(), productCreateFormCaptor.getValue().getDescription()),
-            () -> assertEquals(form.getPrice(), productCreateFormCaptor.getValue().getPrice()),
-            () -> assertEquals(form.getImage().getBytes(), productCreateFormCaptor.getValue().getImage().getBytes())
+            () -> assertEquals(name, productCreateFormCaptor.getValue().getName()),
+            () -> assertEquals(description, productCreateFormCaptor.getValue().getDescription()),
+            () -> assertEquals(price, productCreateFormCaptor.getValue().getPrice()),
+            () -> assertNotNull(productCreateFormCaptor.getValue().getImage())
         );
     }
 
     @Test
     void testGetProductById() throws Exception {
         ProductDetail productDetail = productMapper.toProductDetail(generateRandomProduct(), "/image/path/1.jpg", 3);
-        when(productService.getProductById(anyLong())).thenReturn(productDetail);
+        when(productService.getProductById(anyLong())).thenReturn(Mono.just(productDetail));
         long productId = 13L;
 
-        mockMvc.perform(get("/products/" + productId))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("text/html;charset=UTF-8"))
-            .andExpect(view().name("product"));
+        webTestClient.get()
+            .uri("/products/" + productId)
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.TEXT_HTML)
+            .expectBody(String.class)
+            .consumeWith(response -> {
+                String body = response.getResponseBody();
+                assertNotNull(body);
+                assertTrue(body.contains("product"));
+            });
 
         verify(productService, times(1)).getProductById(productId);
     }
