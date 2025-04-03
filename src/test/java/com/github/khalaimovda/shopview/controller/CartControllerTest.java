db77f441f88c217c637e87012049953ccdcdf034
khalaimovda <halaimov_15@mail.ru>
2025-04-03 11:02:29 +0300

    CartController tests

--- src/test/java/com/github/khalaimovda/shopview/controller/CartControllerTest.java
+++ src/test/java/com/github/khalaimovda/shopview/controller/CartControllerTest.java
@@ -2,41 +2,56 @@ package com.github.khalaimovda.shopview.controller;
 
 
 import com.github.khalaimovda.shopview.dto.OrderDetail;
+import com.github.khalaimovda.shopview.dto.OrderWithProducts;
+import com.github.khalaimovda.shopview.model.Order;
+import com.github.khalaimovda.shopview.model.Product;
 import com.github.khalaimovda.shopview.service.CartService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
-import java.util.Optional;
+import java.util.List;
 
-import static com.github.khalaimovda.shopview.utils.OrderUtils.generateRandomActiveOrder;
-import static com.github.khalaimovda.shopview.utils.OrderUtils.getOrderDetail;
+import static com.github.khalaimovda.shopview.utils.OrderUtils.*;
 import static com.github.khalaimovda.shopview.utils.ProductUtils.generateRandomProducts;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@WebMvcTest(CartController.class)
+@WebFluxTest(CartController.class)
 public class CartControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
     @MockitoBean
     private CartService cartService;
 
     @Test
     void testGetCart() throws Exception {
-        OrderDetail orderDetail =  getOrderDetail(generateRandomActiveOrder(generateRandomProducts(5)));
-        when(cartService.getCart()).thenReturn(Optional.of(orderDetail));
+        List<Product> products = generateRandomProducts(5);
+        Order order = generateRandomNotActiveOrder();
+        OrderWithProducts orderWithProducts = generateRandomOrderWithProducts(order.getId(), products);
+        OrderDetail orderDetail = getOrderDetail(orderWithProducts);
+        when(cartService.getCart()).thenReturn(Mono.just(orderDetail));
 
-        mockMvc.perform(get("/cart"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType("text/html;charset=UTF-8"))
-            .andExpect(view().name("cart"));
+        webTestClient
+            .get()
+            .uri("/cart")
+            .exchange()
+            .expectStatus().isOk()
+            .expectHeader().contentType(MediaType.TEXT_HTML)
+            .expectBody(String.class)
+            .consumeWith(response -> {
+               String body = response.getResponseBody();
+               assertNotNull(body);
+               assertTrue(body.contains("cart"));
+            });
 
         verify(cartService, times(1)).getCart();
     }
@@ -44,9 +59,13 @@ public class CartControllerTest {
     @Test
     void testAddProductToCart() throws Exception {
         long productId = 13L;
+        when(cartService.addProductToCart(anyLong())).thenReturn(Mono.empty());
 
-        mockMvc.perform(post("/cart/add/" + productId))
-            .andExpect(status().isOk());
+        webTestClient
+            .post()
+            .uri("/cart/add/" + productId)
+            .exchange()
+            .expectStatus().isOk();
 
         verify(cartService, times(1)).addProductToCart(productId);
     }
@@ -54,9 +73,13 @@ public class CartControllerTest {
     @Test
     void testDecreaseProductInCart() throws Exception {
         long productId = 13L;
+        when(cartService.decreaseProductInCart(anyLong())).thenReturn(Mono.empty());
 
-        mockMvc.perform(post("/cart/decrease/" + productId))
-            .andExpect(status().isOk());
+        webTestClient
+            .post()
+            .uri("/cart/decrease/" + productId)
+            .exchange()
+            .expectStatus().isOk();
 
         verify(cartService, times(1)).decreaseProductInCart(productId);
     }
@@ -64,17 +87,26 @@ public class CartControllerTest {
     @Test
     void testRemoveProductFromCart() throws Exception {
         long productId = 13L;
+        when(cartService.removeProductFromCart(anyLong())).thenReturn(Mono.empty());
 
-        mockMvc.perform(delete("/cart/remove/" + productId))
-            .andExpect(status().isOk());
+        webTestClient
+            .delete()
+            .uri("/cart/remove/" + productId)
+            .exchange()
+            .expectStatus().isOk();
 
         verify(cartService, times(1)).removeProductFromCart(productId);
     }
 
     @Test
     void testCheckout() throws Exception {
-        mockMvc.perform(post("/cart/checkout"))
-            .andExpect(status().isOk());
+        when(cartService.checkout()).thenReturn(Mono.empty());
+
+        webTestClient
+            .post()
+            .uri("/cart/checkout")
+            .exchange()
+            .expectStatus().isOk();
 
         verify(cartService, times(1)).checkout();
     }
