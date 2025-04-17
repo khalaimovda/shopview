d7db111d51d4d710bb53b4c95d7c92783dcc7395
khalaimovda <halaimov_15@mail.ru>
2025-04-18 01:38:41 +0300

    BalanceService unit tests

--- paymentservice/src/test/java/com/github/khalaimovda/shopview/paymentservice/service/BalanceServiceTest.java
+++ paymentservice/src/test/java/com/github/khalaimovda/shopview/paymentservice/service/BalanceServiceTest.java
@@ -1,10 +1,12 @@
 package com.github.khalaimovda.shopview.paymentservice.service;
 
-import com.github.khalaimovda.shopview.paymentservice.domain.Balance;
 import com.github.khalaimovda.shopview.paymentservice.exception.InsufficientFundsException;
+import com.github.khalaimovda.shopview.paymentservice.model.Balance;
 import com.github.khalaimovda.shopview.paymentservice.repository.BalanceRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,6 +15,7 @@ import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -25,47 +28,123 @@ public class BalanceServiceTest {
     @InjectMocks
     private BalanceServiceImpl service;
 
+    @Captor
+    private ArgumentCaptor<Balance> balanceCaptor;
+
     @Test
     void testGetBalance() {
-        Balance actualBalance = new Balance().balance(BigDecimal.TEN);
-        when(repository.getBalance()).thenReturn(Mono.just(actualBalance));
+        long userId = 13L;
+        Balance actualBalance = new Balance();
+        actualBalance.setId(15L);
+        actualBalance.setBalance(BigDecimal.TEN);
+        actualBalance.setUserId(userId);
+        when(repository.findByUserId(anyLong())).thenReturn(Mono.just(actualBalance));
 
         StepVerifier
-            .create(service.getBalance())
+            .create(service.getBalance(userId))
             .assertNext(balance -> assertEquals(actualBalance, balance))
             .verifyComplete();
 
-        verify(repository, times(1)).getBalance();
+        verify(repository, times(1)).findByUserId(userId);
     }
 
     @Test
     void testDecreaseBalance() {
-        Balance actualBalance = new Balance().balance(BigDecimal.TEN);
-        when(repository.getBalance()).thenReturn(Mono.just(actualBalance));
+        long userId = 13L;
+        Balance actualBalance = new Balance();
+        actualBalance.setId(15L);
+        actualBalance.setBalance(BigDecimal.TEN);
+        actualBalance.setUserId(userId);
+        when(repository.findByUserId(anyLong())).thenReturn(Mono.just(actualBalance));
         BigDecimal amount = BigDecimal.TWO;
-        Balance newBalance = new Balance().balance(actualBalance.getBalance().subtract(amount));
-        when(repository.updateBalance(any(BigDecimal.class))).thenReturn(Mono.just(newBalance));
+
+        Balance newBalance = new Balance();
+        newBalance.setId(actualBalance.getId());
+        newBalance.setUserId(actualBalance.getUserId());
+        newBalance.setBalance(actualBalance.getBalance().subtract(amount));
+
+        when(repository.save(any(Balance.class))).thenReturn(Mono.just(newBalance));
 
         StepVerifier
-            .create(service.decreaseBalance(amount))
+            .create(service.decreaseBalance(userId, amount))
             .assertNext(balance -> assertEquals(newBalance, balance))
             .verifyComplete();
 
-        verify(repository, times(1)).getBalance();
-        verify(repository, times(1)).updateBalance(newBalance.getBalance());
+        verify(repository, times(1)).findByUserId(userId);
+        verify(repository, times(1)).save(balanceCaptor.capture());
+
+        assertEquals(newBalance, balanceCaptor.getValue());
     }
 
     @Test
     void testDecreaseBalanceInsufficientFunds() {
-        Balance actualBalance = new Balance().balance(BigDecimal.TWO);
-        when(repository.getBalance()).thenReturn(Mono.just(actualBalance));
+        long userId = 13L;
+        Balance actualBalance = new Balance();
+        actualBalance.setId(15L);
+        actualBalance.setBalance(BigDecimal.TWO);
+        actualBalance.setUserId(userId);
+        when(repository.findByUserId(anyLong())).thenReturn(Mono.just(actualBalance));
         BigDecimal amount = BigDecimal.TEN;
 
         StepVerifier
-            .create(service.decreaseBalance(amount))
+            .create(service.decreaseBalance(userId, amount))
             .expectError(InsufficientFundsException.class)
             .verify();
 
-        verify(repository, times(1)).getBalance();
+        verify(repository, times(1)).findByUserId(userId);
+    }
+
+    @Test
+    void testAddBalanceNewBalance() {
+        long userId = 13L;
+        BigDecimal amount = BigDecimal.TEN;
+        Balance newBalance = new Balance();
+        newBalance.setBalance(amount);
+        newBalance.setUserId(userId);
+        when(repository.findByUserId(anyLong())).thenReturn(Mono.empty());
+        when(repository.save(any(Balance.class))).thenReturn(Mono.just(newBalance));
+
+        StepVerifier
+            .create(service.addBalance(userId, amount))
+            .assertNext(balance -> assertAll(
+                () -> assertEquals(newBalance.getUserId(), balance.getUserId()),
+                () -> assertEquals(newBalance.getBalance(), balance.getBalance())
+            ))
+            .verifyComplete();
+
+        verify(repository, times(1)).findByUserId(userId);
+        verify(repository, times(1)).save(balanceCaptor.capture());
+
+        assertEquals(newBalance, balanceCaptor.getValue());
+    }
+
+
+    @Test
+    void testAddBalanceExistingBalance() {
+        long userId = 13L;
+        BigDecimal amount = BigDecimal.TEN;
+
+        Balance actualBalance = new Balance();
+        actualBalance.setId(15L);
+        actualBalance.setBalance(BigDecimal.TWO);
+        actualBalance.setUserId(userId);
+
+        Balance newBalance = new Balance();
+        newBalance.setId(actualBalance.getId());
+        newBalance.setUserId(actualBalance.getUserId());
+        newBalance.setBalance(actualBalance.getBalance().add(amount));
+
+        when(repository.findByUserId(anyLong())).thenReturn(Mono.just(actualBalance));
+        when(repository.save(any(Balance.class))).thenReturn(Mono.just(newBalance));
+
+        StepVerifier
+            .create(service.addBalance(userId, amount))
+            .assertNext(balance -> assertEquals(newBalance, balance))
+            .verifyComplete();
+
+        verify(repository, times(1)).findByUserId(userId);
+        verify(repository, times(1)).save(balanceCaptor.capture());
+
+        assertEquals(newBalance, balanceCaptor.getValue());
     }
 }
